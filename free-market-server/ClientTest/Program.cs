eb3a84abd5947dee74ac6fd99a6b7ce207b16d67
MateoGiraz <mateogiraz27@gmail.com>
2023-09-01 22:01:50 -0300

    include server logic testing env

---
+++ free-market-server/ClientTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using CoreBusiness;
+using BusinessLogic;
+
+/* *** DISCLAIMER ***
+ Solo existe la relacion con CoreBusiness para testear. Cuando esto funcione no necesitamos
+ el objeto producto, sino que vamos a representarlo como json o algo del estilo xd.
+ */
+
+ProductController pr = new();
+
+var patata = new Product()
+{
+    Name = "Patata",
+    Description = "Es una patata bro",
+    Price = 10,
+    Stock = 5,
+};
+
+pr.AddProduct(patata);
+
+pr.BuyProduct(patata, 3);
+
+var list = pr.GetProducts();
+
+foreach (var product in list)
+{
+    Console.WriteLine("There are " + product.Stock + " " + product.Name);
+}
