1b11f8ef116dfea6f9f998b24e36d4abe2ba588c
MateoGiraz <mateogiraz27@gmail.com>
2023-09-22 14:06:44 -0300

    include server handler

--- FreeMarketServer/ServerConnection/OptionHandler.cs
+++ FreeMarketServer/ServerConnection/OptionHandler.cs
@@ -2,6 +2,7 @@
 using Common.Helpers;
 using Common.Protocol;
 using System.Net.Sockets;
+using ServerConnection.Handler;
 
 
 namespace ServerConnection;
@@ -20,45 +21,13 @@ internal class OptionHandler
         switch (option)
         {
             case 1:
-                HandleUserCreation();
+                UserCreationHandler.Handle(_socket);
                 break;
             case 2:
-                HandlePicReceiving();
+                PicReceivingHandler.Handle(_socket);
                 break;
             default:
                 break;
         }
     }
-
-    private void HandleUserCreation()
-    {
-        var (bytesRead, messageLength) =
-            NetworkHelper.ReceiveIntData(ProtocolStandards.SizeMessageDefinedLength, _socket);
-
-        if (bytesRead == 0)
-            return;
-
-        (bytesRead, var userString) = NetworkHelper.ReceiveStringData(messageLength, _socket);
-
-        if (bytesRead == 0)
-            return;
-
-        var userMap = KOI.Parse(userString);
-
-        var userDTO = new UserDTO()
-        {
-            UserName = userMap["UserName"].ToString(),
-            Password = userMap["Password"].ToString()
-        };
-
-        Console.WriteLine("Received Username: " + userDTO.UserName);
-        Console.WriteLine("Received Password: " + userDTO.Password);
-    }
-
-    private void HandlePicReceiving()
-    {
-        var fileTransferHelper = new FileTransferHelper();
-        fileTransferHelper.ReceiveFile(_socket);
-    }
-
 }
