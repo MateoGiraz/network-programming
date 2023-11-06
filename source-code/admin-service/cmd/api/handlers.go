cafcdddfcec7d9dc9ada7fd944a85032ba808bd3
MateoGiraz <santiagovillar2002@gmail.com>
2023-11-06 15:44:22 -0300

    create admin service

---
+++ source-code/admin-service/cmd/api/handlers.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (app *Config) createProduct(w http.ResponseWriter, r *http.Request) {}
+func (app *Config) updateProduct(w http.ResponseWriter, r *http.Request) {}
+func (app *Config) deleteProduct(w http.ResponseWriter, r *http.Request) {}
+func (app *Config) buyProduct(w http.ResponseWriter, r *http.Request)    {}
+func (app *Config) getRating(w http.ResponseWriter, r *http.Request)     {}
---
+++ source-code/admin-service/go.mod
@@ -0,0 +1,8 @@
+module github.com/ORT-PDR/M6C_241195_256345_231355/admin-service
+
+go 1.19
+
+require (
+	github.com/go-chi/chi/v5 v5.0.10
+	github.com/go-chi/cors v1.2.1
+)
